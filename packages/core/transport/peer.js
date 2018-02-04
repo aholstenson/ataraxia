ff0194b9a122d8c17e626465bb320f5aae0f449d
Andreas Holstenson <a@holstenson.se>
2018-02-04 09:34:41 +0100

    Docs and tweaks for AbstractTransport and Peer

--- packages/core/transport/peer.js
+++ packages/core/transport/peer.js
@@ -8,7 +8,29 @@ const msgpack = require('msgpack-lite');
 const CURRENT_VERSION = 2;
 const pingInterval = 5000;
 
+/**
+ * Peer as found by a transport. Peers represent a connection between the
+ * local transport instance and another transprt instance. Peers can be remote
+ * computers, within the same VM or machine local.
+ *
+ * This class can be extended by transports as needed.
+ *
+ * The following events are available on a peer:
+ *
+ * * `connected` - emitted when the peer is connected
+ * * `disconnected` - emitted when the peer is disconnected
+ * * `message` - when a message is received from the peer
+ * * `ping` - Internal use - when a ping is received from the peer
+ * * `hello` - Internal use - when initial negotiation is done
+ *
+ */
 module.exports = class Peer {
+
+	/**
+	 * Create a new peer over the given transport.
+	 *
+	 * @param {AbstractTransport} transport
+	 */
 	constructor(transport) {
 		this.networkId = transport.networkId;
 		const ns = transport.debug ? transport.debug.namespace + ':peer' : 'ataraxia:peer';
@@ -18,24 +40,27 @@ module.exports = class Peer {
 
 		// Reply to hello messages with our metadata
 		this.events.on('hello', msg => {
+			// Set the identifier and the version of the protocol to use
 			this.id = msg.id;
 			this.version = Math.min(msg.version, CURRENT_VERSION);
 
+			// Update debugging with the identifier of the peer
 			this.debug = debug(ns + ':' + msg.id);
 
+			// Clear the timeout that decides when a peer fails negotiation
 			if(this.helloTimeout) {
 				clearTimeout(this.helloTimeout);
 				this.helloTimeout = null;
 			}
 
 			if(this.version >= 2) {
-				// Setup a ping every 5 seconds
+				// V2+ of protocol - setup a ping every 5 seconds
 				this.pingSender = setInterval(() => this.write('ping'), pingInterval);
 				this.pingTimeout = setTimeout(() => this.socket && this.socket.destroy(), pingInterval * 4);
 
 				this.write('ping');
 			} else {
-				// Assume we are connected when hello is received
+				// V1 of protocol - assume we are connected when hello is received
 				this.events.emit('connected');
 			}
 		});
@@ -56,12 +81,16 @@ module.exports = class Peer {
 		});
 	}
 
+	/**
+	 * Get if a socket is available.
+	 */
 	hasSocket() {
 		return this.socket != null;
 	}
 
 	/**
-	 * Set the socket
+	 * Set the socket being used for this peer.
+	 *
 	 * @param {Socket} s
 	 */
 	setSocket(s) {
@@ -90,6 +119,12 @@ module.exports = class Peer {
 		pipe.on('error', err => this.debug('Error from pipe', err));
 	}
 
+	/**
+	 * Handle disconnect event. This implementation will log info about the
+	 * disconnect and then mark the peer as disconnected.
+	 *
+	 * Transports may override this to provide reconnection behavior.
+	 */
 	handleDisconnect(err) {
 		if(typeof err !== 'undefined') {
 			this.debug('Disconnected via an error:', err);
@@ -109,6 +144,10 @@ module.exports = class Peer {
 		this.events.emit('disconnected');
 	}
 
+	/**
+	 * Negotiate with this peer to retrieve information about it. Will send a
+	 * hello message with our network id and the maximum version we can handle.
+	 */
 	negotiate() {
 		// Write the hello message
 		this.write('hello', {
@@ -123,10 +162,16 @@ module.exports = class Peer {
 		this.helloTimeout = setTimeout(() => this.socket.destroy(), 5000);
 	}
 
+	/**
+	 * Register an event handler.
+	 */
 	on(event, handler) {
 		this.events.on(event, handler);
 	}
 
+	/**
+	 * Manually disconnect this peer.
+	 */
 	disconnect() {
 		this.debug('Requesting disconnect from peer');
 
@@ -138,10 +183,20 @@ module.exports = class Peer {
 		}
 	}
 
+	/**
+	 * Send a message to this peer.
+	 */
 	send(payload) {
 		this.write('message', payload);
 	}
 
+	/**
+	 * Write data to the peer, used internally to send data associated with a
+	 * specific type. Use `send` unless extending the Peer class.
+	 *
+	 * @param {string} type
+	 * @param {*} payload
+	 */
 	write(type, payload) {
 		this.debug('Sending', type, 'with data', payload);
 		const data = msgpack.encode([ String(type), payload ]);
