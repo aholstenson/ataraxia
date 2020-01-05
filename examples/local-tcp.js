e2d2f35ac0cafdf6eee6737940bdb8cae552bd27
Andreas Holstenson <a@holstenson.se>
2020-01-05 11:36:46 +0100

    chore: Update local-tcp example to use same helper as other examples

--- examples/local-tcp.js
+++ examples/local-tcp.js
@@ -29,13 +29,9 @@ localTransport.onLeader(() => {
 });
 net.addTransport(localTransport);
 
-// A counter that is sent to other nodes
-let counter = 0;
-
 // Log when new nodes are available and send them a hello with the counter
 net.onNodeAvailable(node => {
 	console.log('Node', node, 'is now available');
-	node.send('hello', { counter: counter });
 });
 
 net.onNodeUnavailable(node => {
@@ -47,12 +43,12 @@ net.onMessage(msg => {
 	console.log('Message was received:', 'type=', msg.type, 'data=', msg.data, 'source=', msg.source);
 });
 
-// Increment and broadcast counter to all nodes every five seconds
-setInterval(() => {
-	net.broadcast('counter', { current: ++counter });
-}, 5000);
-
 // Start the network
 net.start()
-	.then(() => console.log('Network has started with id', net.networkId))
+	.then(() => {
+		console.log('Network has started with id', net.networkId);
+
+		// Start our helper
+		return require('./helpers/counter')(net);
+	})
 	.catch(err => console.error(err));
