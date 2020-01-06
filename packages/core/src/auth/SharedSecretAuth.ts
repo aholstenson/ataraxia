e4969175953c9e6e85f59f973bfb732bc8412c5f
Andreas Holstenson <a@holstenson.se>
2020-01-06 16:27:14 +0100

    feat(core): SharedSecretAuth now uses a challenge response flow
    `SharedSecretAuth` has been rewritten to use cryptographic functions to
    verify that both the client and server node has the same shared secret.
    
    This is done by requesting that the other peer signs a 32 byte randomly
    generated challenge using HMAC-SHA256.

--- packages/core/src/auth/SharedSecretAuth.ts
+++ packages/core/src/auth/SharedSecretAuth.ts
@@ -1,9 +1,13 @@
+import { Encoder, Decoder } from '@stablelib/cbor';
+import { hmac } from 'fast-sha256';
+import { TextEncoder } from 'fastestsmallesttextencoderdecoder';
+
 import { AuthProvider } from './AuthProvider';
 import { AuthClientFlow } from './AuthClientFlow';
 import { AuthServerFlow, AuthServerReplyType } from './AuthServerFlow';
 
 export interface SharedSecretAuthOptions {
-	secret: ArrayBuffer;
+	secret: ArrayBuffer | string;
 }
 
 /**
@@ -16,18 +20,55 @@ export class SharedSecretAuth implements AuthProvider {
 	private readonly secret: ArrayBuffer;
 
 	constructor(options: SharedSecretAuthOptions) {
-		this.secret = options.secret;
+		if(typeof options.secret === 'string') {
+			const encoder = new TextEncoder();
+			this.secret = encoder.encode(options.secret);
+		} else {
+			this.secret = options.secret;
+		}
 	}
 
 	public createClientFlow(): AuthClientFlow {
 		const secret = this.secret;
+		const challenge = randomBytes(32);
 		return {
-			initialMessage() {
-				return Promise.resolve(secret);
+			async initialMessage() {
+				const encoder = new Encoder();
+
+				// Version indicator
+				encoder.encodeInteger(1);
+
+				// Challenge
+				encoder.encodeBytes(new Uint8Array(challenge));
+
+				// Return the initial message
+				return encoder.finish().buffer;
 			},
 
-			receiveData() {
-				return Promise.reject();
+			async receiveData(data) {
+				const decoder = new Decoder(new Uint8Array(data), {
+					ignoreExtraData: true
+				});
+
+				const version = decoder.decode();
+				if(version !== 1) {
+					throw new Error('Unknown version');
+				}
+
+				const serverChallenge: Uint8Array = decoder.decode();
+				const serverResponse: Uint8Array = decoder.decode();
+
+				// Calculate what the expected response is and compare them
+				const calculatedResponse = calculateResponse(secret, challenge);
+				if(! isEqual(serverResponse, calculatedResponse)) {
+					throw new Error('Server did not correctly verify its identity, check that the same secret is in use');
+				}
+
+				// Calculate our response to the servers challenge
+				const response = calculateResponse(secret, serverChallenge);
+				const encoder = new Encoder();
+				encoder.encodeBytes(response);
+				return encoder.finish().buffer;
 			},
 
 			destroy() {
@@ -38,15 +79,60 @@ export class SharedSecretAuth implements AuthProvider {
 
 	public createServerFlow(): AuthServerFlow {
 		const secret = this.secret;
+		const challenge = randomBytes(32);
 		return {
-			receiveInitial(data: ArrayBuffer) {
-				return Promise.resolve({
-					type: isEqual(secret, data) ? AuthServerReplyType.Ok : AuthServerReplyType.Reject
+			async receiveInitial(data: ArrayBuffer) {
+				const decoder = new Decoder(new Uint8Array(data), {
+					ignoreExtraData: true
 				});
+
+				const version = decoder.decode();
+				if(version !== 1) {
+					return {
+						type: AuthServerReplyType.Reject
+					};
+				}
+
+				const clientChallenge = decoder.decode();
+
+				const encoder = new Encoder();
+
+				// Version indicator
+				encoder.encodeInteger(1);
+
+				// Challenge
+				encoder.encodeBytes(challenge);
+
+				// Challenge reply
+				const response = calculateResponse(secret, clientChallenge);
+				encoder.encodeBytes(response);
+
+				// Return the initial message
+				return {
+					type: AuthServerReplyType.Data,
+
+					data: encoder.finish().buffer
+				};
 			},
 
-			receiveData() {
-				return Promise.reject();
+			async receiveData(data) {
+				const decoder = new Decoder(new Uint8Array(data), {
+					ignoreExtraData: true
+				});
+
+				const clientResponse: Uint8Array = decoder.decode();
+
+				// Calculate what the expected response is and compare them
+				const calculatedResponse = calculateResponse(secret, challenge);
+				if(! isEqual(clientResponse, calculatedResponse)) {
+					return {
+						type: AuthServerReplyType.Reject
+					};
+				}
+
+				return {
+					type: AuthServerReplyType.Ok
+				};
 			},
 
 			destroy() {
@@ -61,16 +147,48 @@ export class SharedSecretAuth implements AuthProvider {
  * and does not short circuit so that the timing of calls are as similar as
  * possible.
  */
-function isEqual(o1: ArrayBuffer, o2: ArrayBuffer): boolean {
+function isEqual(o1: Uint8Array, o2: Uint8Array): boolean {
 	let result = o1.byteLength === o2.byteLength;
 
-	const u1 = new Uint8Array(o1);
-	const u2 = new Uint8Array(o2);
-	for(let i=0, n=Math.max(u1.length, u2.length); i<n; i++) {
-		if(u1[i] !== u2[i] && result) {
+	for(let i=0, n=Math.max(o1.length, o2.length); i<n; i++) {
+		if(o1[i] !== o2[i] && result) {
 			result = false;
 		}
 	}
 
 	return result;
+}
+
+function calculateResponse(key: ArrayBuffer, challenge: Uint8Array) {
+	return hmac(new Uint8Array(key), challenge);
+}
+
+declare const window: any;
+
+/**
+ * Generate a given number of random bytes. This method will try to use the
+ * best available source of randomness:
+ *
+ * * For node it uses `randomBytes` from the `crypto` module
+ * * `window.crypto.getRandomValues()` is used if available
+ * * Fallback to `Math.random` if the above are not available
+ *
+ * @param n
+ */
+function randomBytes(n: number): Uint8Array {
+	if(typeof window === 'undefined') {
+		return require('crypto').randomBytes(n);
+	} else {
+		const crypto = window.crypto;
+		const result = new Uint8Array(n);
+		if(crypto && crypto.getRandomValues) {
+			crypto.getRandomValues(result);
+		} else {
+			for(let i=0; i<n; i++) {
+				result[i] = Math.floor(Math.random() * 256);
+			}
+		}
+
+		return result;
+	}
 }
