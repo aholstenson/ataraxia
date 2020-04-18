9a0c312035146b81c6a587bbf06c6a3e25840003
Andreas Holstenson <a@holstenson.se>
2020-04-18 16:30:42 +0200

    test(core): Add more topology tests

--- packages/core/test/topology/Topology.test.ts
+++ packages/core/test/topology/Topology.test.ts
@@ -26,6 +26,13 @@ describe('Topology', () => {
 		expect(tester.nodes('b')).toEqual([ 'a', 'c' ]);
 		expect(tester.nodes('c')).toEqual([ 'a', 'b' ]);
 
+		expect(tester.path('a', 'b')).toEqual([ 'a' ]);
+		expect(tester.path('a', 'c')).toEqual([ 'a' ]);
+		expect(tester.path('b', 'a')).toEqual([ 'b' ]);
+		expect(tester.path('b', 'c')).toEqual([ 'b' ]);
+		expect(tester.path('c', 'a')).toEqual([ 'c' ]);
+		expect(tester.path('c', 'b')).toEqual([ 'c' ]);
+
 		await tester.shutdown();
 	});
 
@@ -39,6 +46,10 @@ describe('Topology', () => {
 
 			await tester.consolidate();
 
+			expect(tester.nodes('a')).toEqual([ 'b', 'c' ]);
+			expect(tester.nodes('b')).toEqual([ 'a', 'c' ]);
+			expect(tester.nodes('c')).toEqual([ 'a', 'b' ]);
+
 			expect(tester.path('a', 'b')).toEqual([ 'a' ]);
 			expect(tester.path('a', 'c')).toEqual([ 'a' ]);
 			expect(tester.path('b', 'a')).toEqual([ 'b' ]);
@@ -49,6 +60,10 @@ describe('Topology', () => {
 			tester.disconnect('a', 'c');
 
 			await tester.consolidate();
+
+			expect(tester.nodes('a')).toEqual([ 'b', 'c' ]);
+			expect(tester.nodes('b')).toEqual([ 'a', 'c' ]);
+			expect(tester.nodes('c')).toEqual([ 'a', 'b' ]);
 
 			expect(tester.path('a', 'b')).toEqual([ 'a' ]);
 			expect(tester.path('a', 'c')).toEqual([ 'a', 'b' ]);
@@ -61,6 +76,44 @@ describe('Topology', () => {
 		}
 	});
 
+	it('Fully-connected - 4 peers, disconnect one', async () => {
+		const tester = new TopologyTester()
+			.bidirectional('a', 'b')
+			.bidirectional('a', 'c')
+			.bidirectional('a', 'd')
+			.bidirectional('b', 'c')
+			.bidirectional('b', 'd')
+			.bidirectional('c', 'd');
+
+		try {
+
+			await tester.consolidate();
+
+			expect(tester.nodes('a')).toEqual([ 'b', 'c', 'd' ]);
+			expect(tester.nodes('b')).toEqual([ 'a', 'c', 'd' ]);
+			expect(tester.nodes('c')).toEqual([ 'a', 'b', 'd' ]);
+
+			expect(tester.path('a', 'b')).toEqual([ 'a' ]);
+			expect(tester.path('a', 'c')).toEqual([ 'a' ]);
+			expect(tester.path('b', 'a')).toEqual([ 'b' ]);
+			expect(tester.path('b', 'c')).toEqual([ 'b' ]);
+			expect(tester.path('c', 'a')).toEqual([ 'c' ]);
+			expect(tester.path('c', 'b')).toEqual([ 'c' ]);
+
+			tester.disconnect('a', 'd');
+			tester.disconnect('b', 'd');
+			tester.disconnect('c', 'd');
+
+			await tester.consolidate();
+
+			expect(tester.nodes('a')).toEqual([ 'b', 'c' ]);
+			expect(tester.nodes('b')).toEqual([ 'a', 'c' ]);
+			expect(tester.nodes('c')).toEqual([ 'a', 'b' ]);
+		} finally {
+			await tester.shutdown();
+		}
+	});
+
 	it('Mesh - 3 peers, B as router', async () => {
 		const tester = new TopologyTester()
 			.bidirectional('a', 'b')
@@ -68,6 +121,9 @@ describe('Topology', () => {
 
 		try {
 			await tester.consolidate();
+
+			expect(tester.nodes('a')).toEqual([ 'b', 'c' ]);
+			expect(tester.nodes('b')).toEqual([ 'a', 'c' ]);
 
 			expect(tester.path('a', 'b')).toEqual([ 'a' ]);
 			expect(tester.path('a', 'c')).toEqual([ 'a', 'b' ]);
@@ -106,6 +162,30 @@ describe('Topology', () => {
 
 			expect(tester.path('d', 'a')).toEqual([ 'd', 'b' ]);
 			expect(tester.path('d', 'b')).toEqual([ 'd' ]);
+		} finally {
+			await tester.shutdown();
+		}
+	});
+
+	it('Mesh - 5 peers, disconnect', async () => {
+		const tester = new TopologyTester()
+			.bidirectional('a', 'b')
+			.bidirectional('a', 'c')
+			.bidirectional('b', 'd')
+			.bidirectional('b', 'e');
+
+		try {
+			await tester.consolidate();
+
+			tester.disconnect('a', 'c');
+
+			await tester.consolidate();
+
+			expect(tester.nodes('a')).toEqual([ 'b', 'd', 'e' ]);
+			expect(tester.nodes('b')).toEqual([ 'a', 'd', 'e' ]);
+			expect(tester.nodes('c')).toEqual([]);
+			expect(tester.nodes('d')).toEqual([ 'a', 'b', 'e' ]);
+			expect(tester.nodes('e')).toEqual([ 'a', 'b', 'd' ]);
 		} finally {
 			await tester.shutdown();
 		}
