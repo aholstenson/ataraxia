a06eae2adcd4f85b35085dad41979bead20de3b4
Andreas Holstenson <a@holstenson.se>
2021-06-09 14:15:03 +0200

    docs(core): Add some basic module docs

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,46 @@
 /* eslint-disable tsdoc/syntax,jsdoc/require-description */
 /**
+ * Mesh networking with peer-to-peer messaging for NodeJS and the browser.
+ * Ataraxia connects different instances together and allows messages to be passed
+ * between these instances. Some instances may act as routers for other instances
+ * to create a partially connected mesh network.
+ *
+ * {@link Network} is the main class used to join a network:
+ *
+ * ```javascript
+ * import { Network, AnonymousAuth } from 'ataraxia';
+ * import { TCPTransport, TCPPeerMDNSDiscovery } from 'ataraxia-tcp';
+ *
+ * // Setup a network with a TCP transport
+ * const net = new Network({
+ *   name: 'name-of-your-app-or-network',
+ *   transports: [
+ *     new TCPTransport({
+ *       // Discover peers using mDNS
+ *       discovery: new TCPPeerMDNSDiscovery(),
+ *       // Setup anonymous authentication
+ *       authentication: [
+ *         new AnonymousAuth()
+ *       ]
+ *     })
+ *   ]
+ * });
+ *
+ * net.onNodeAvailable(node => {
+ *   console.log('A new node is available', node);
+ * });
+ *
+ * net.onMessage(msg => {
+ *   console.log('A new message was received');
+ * });
+ *
+ * // Start the network
+ * await net.start();
+ * ```
+ *
+ * Authentication is provided via {@link AnonymousAuth} or
+ * {@link SharedSecretAuth}.
+ *
  * @module ataraxia
  */
 
