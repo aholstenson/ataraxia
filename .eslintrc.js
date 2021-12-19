562f774f2c6da28066267e92026674f998c1bc99
Andreas Holstenson <a@holstenson.se>
2021-12-19 10:33:11 +0100

    build: Update to ESLint 5

--- .eslintrc.js
+++ .eslintrc.js
@@ -178,7 +178,6 @@ module.exports = {
 
 		'tsdoc/syntax': [ 'warn' ],
 		'jsdoc/check-alignment': [ 'warn' ],
-		'jsdoc/check-examples': [ 'warn' ],
 		'jsdoc/check-param-names': [ 'warn', {
 			checkDestructured: false
 		} ],
